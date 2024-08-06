80656ab80698f55d1fd03bf26368b97f443eb71d
Niklas Daute <niklasdaute@icloud.com>
2024-08-06 13:12:51 +0200

    Updates

---
+++ precache-manifest.f413983ce64544df5b62a6b26192a513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28f9d2c08eac4195a240bd8f70c3c69",
+    "url": "/auth-app/index.html"
+  },
+  {
+    "revision": "f4fbdd4d781177aee4b7",
+    "url": "/auth-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "74a22a0132f7e04229be",
+    "url": "/auth-app/static/js/2.2f63d9d7.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/auth-app/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "f4fbdd4d781177aee4b7",
+    "url": "/auth-app/static/js/main.3a4cbc8d.chunk.js"
+  },
+  {
+    "revision": "a5642336247c417063bb",
+    "url": "/auth-app/static/js/runtime~main.6afa407c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/auth-app/static/media/logo.06e73328.svg"
+  }
+]);
