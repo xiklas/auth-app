141feddd9c68af021fedfde9b8b84b05f4a93783
Niklas Daute <niklasdaute@icloud.com>
2024-08-06 13:08:42 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Auth from '../components/auth';
+import Auth from './components/auth';
 
 function App() {
   return (
